32cc95783498dca4e4af0f5f2fb4a3294601f43f
David Morán <david.morandz@gmail.com>
2014-08-08 17:45:42 +0200

    Modificado main para usar la nueva función para crear los XML

--- create_flame.py
+++ create_flame.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 import argparse
-from utils import *
-from numpy import arange
 from lxml import etree
 from render import render_flame_file
-from itertools import product
+from xml_generator import generate_xml_flame
+
 
 parser = argparse.ArgumentParser(description='Create a flame file with a name')
 parser.add_argument('name', help=u'Nombre para el archivo de flames')
@@ -12,64 +11,24 @@ parser.add_argument('name', help=u'Nombre para el archivo de flames')
 args = parser.parse_args()
 print('Nombre = ' + args.name)
 
-# Data values, ranges and variation definition
-npartes = 2.0
-variation_names = []
+# Data ranges, samples and variation definition
+data = [
+    [1, 7, 1, 'swirl'], # N - 9 + 1
+    [0.1, 1.3, 8, 'julia'], # N + 0.1
+    [5, 12, 1, 'sinusoidal'] # N/10
+]
+name = 'prueba-con-funcion'
 
-alcoholMin = 0.26
-alcoholMax = 1.09
-alcoholPaso = (alcoholMax - alcoholMin) / npartes
-variation_names.append('swirl')
-
-acidezMin = 0.3
-acidezMax = 1.8
-acidezPaso = (acidezMax - acidezMin) / npartes
-variation_names.append('julia')
-
-sulfMin = 3.4
-sulfMax = 6.6
-sulfPaso = (sulfMax - sulfMin) / npartes
-variation_names.append('sinusoidal')
-
-# Get all the ranges and convert them to a list to avoid precision errors
-rng_alcohol = arange(alcoholMin, alcoholMax + alcoholPaso, alcoholPaso).tolist()
-rng_acidez = arange(acidezMin, acidezMax + acidezPaso, acidezPaso).tolist()
-rng_sulf = arange(sulfMin, sulfMax + sulfPaso, sulfPaso).tolist()
-
-print('rango alcohol = ' + str(rng_alcohol))
-print('rango acidez = ' + str(rng_acidez))
-print('rango sulfuroso = ' + str(rng_sulf))
-print('')
-
-# Create the xml for the flame file:
-# Get all the combinations for the ranges
-data = list(product(rng_alcohol, rng_acidez, rng_sulf))
-print('data = ' + str(data))
-
-# Create the XML tree for the .flame file
-# root
-Flames = etree.Element('Flames', name=args.name)
-i = 0
-for flame_tuple in data:
-    # flame child
-    flame = etree.SubElement(Flames, 'flame', flame_properties(str(i)))
-    i += 1
-
-    for i, value in enumerate(flame_tuple):
-        # transform child (triangles in apophysis)
-        xform = etree.SubElement(flame, 'xform', xform_properties(variation_names[i], value, 0))
-
-    # And add a default gradient
-    gradient = etree.SubElement(flame, 'palette', gradient_properties())
-    gradient.text = gradient_content()
+# Pass the data to the XML-Flame generator
+flames = generate_xml_flame(data, name)
 
 # Print it to screen!
-print(etree.tostring(Flames, pretty_print=True))
+print(etree.tostring(flames, pretty_print=True))
 
 # Save the XML .flame to a file
-f = open('testing.flame', 'w')
-f.write(etree.tostring(Flames, pretty_print=True))
+f = open(name + '.flame', 'w')
+f.write(etree.tostring(flames, pretty_print=True))
 f.close()
 
 # Call the render function to render and process all the flames
-render_flame_file('testing.flame', args.name)
+render_flame_file(name + '.flame', name)
