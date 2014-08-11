88b0e855bdcb163242fee1736f5aa95f4c7f204f
David Morán <david.morandz@gmail.com>
2014-08-11 18:43:44 +0200

    Añadido parametro real_name a la hora de crear el xml de flames para asociarle un parámetro en el mundo real. Es ignorado por flam3.

--- xml_generator.py
+++ xml_generator.py
@@ -7,57 +7,62 @@ from itertools import product
 
 def generate_xml_flame(data_list, file_name):
     """
-    Receives a list of lists with the next structure:
+    Recibe una lista con la siguiente estructura:
 
         [ [min1, max1, samples1, variation_name1], [min1, max1, samples1, variation_name1], ...]
 
-    Where each sublist item must be:
-        min: Minimum of the range. Could be a float.
-        max: Maximum of the range. Could be a float.
-        samples: Number of equal parts that the range must be divided on. Should be an integer.
-        variation_name: Name of the variation to apply to the transform (ex: linear, sinusoidal, swirl...). Must be a string.
+    Donde los elementos de cada sublista son:
+        min: El mínimo del rango. Puede ser de tipo float.
+        max: El Máximo del rango. Puede ser de tipo float.
+        samples: Número de partes iguales en las que debe dividirse el rango. Should be an integer.
+        variation_name: Nombre de la variación a aplicar a la transformada (ex: linear, sinusoidal, swirl...).
+                        Debe ser una cadena.
 
-    Return a etree object with the generated xml.
+    Devuelve un objeto etree con el XML generado
 
-    :param data_list: list of lists
-    :param file_name: name of the flame file to output
+    :param data_list: lista de listas
+    :param file_name: nombre del archivo de flames
     :return: etree
     """
 
-    ranges = []  # List of all the input ranges
-    variation_names = []  # List of all the variation names
+    ranges = []  # Lista de todos los rangos
+    variation_names = []  # Lista de todas los nombres de las variaciones
+    parameter_names = []  # Lista de todos los parámetros (de la vida real) a los que se corresponde la variacion
 
-    # Process the data list
+    # Procesar la lista de datos
     for element in data_list:
-        # Get the range of data for actual element
-        # var declaration for easy understanding (could be omitted)
+        # Obtener el rango de los datos para el elemento actual
+        # definicion de variables por claridad, puede omitirse
         minimum = element[0]
         maximum = element[1]
         samples = element[2]
+        var_name = element[3]
+        param_name = element[4]
 
-        step = (maximum - minimum) / float(samples)  # Calculate the step for arange function
+        step = (maximum - minimum) / float(samples)  # Calcular el paso para la funcion arange
         ranges.append(arange(minimum, maximum, step).tolist())
 
-        # Copy the name to another list
-        variation_names.append(element[3])
+        # Copiar el nombre de la variación y el parámetro a otras dos listas
+        variation_names.append(var_name)
+        parameter_names.append(param_name)
 
     all_ranges = list(product(*ranges))
     print('data = ' + str(all_ranges))
 
-    # Create the XML tree for the .flame file
-    # root
+    # Crear el árbol XML para el archivo .flame
+    # raíz
     flames = etree.Element('Flames', name=file_name)
     i = 0
     for range_tuple in all_ranges:
-        # flame child
+        # flame hijo
         flame = etree.SubElement(flames, 'flame', flame_properties(str(i)))
         i += 1
 
         for i, value in enumerate(range_tuple):
-            # transform child (triangles in apophysis)
-            xform = etree.SubElement(flame, 'xform', xform_properties(variation_names[i], value, i))
+            # transformada hija (triangulo en apophysis)
+            xform = etree.SubElement(flame, 'xform', xform_properties(variation_names[i], value, i, parameter_names[i]))
 
-        # And add a default gradient
+        # Añadir un gradiente fijo por defecto
         gradient = etree.SubElement(flame, 'palette', gradient_properties())
         gradient.text = gradient_content()
 
