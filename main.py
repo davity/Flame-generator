eef21f30fb93aa56d59ab5738254376b2b3dbd5b
David Morán <david.morandz@gmail.com>
2014-09-02 13:14:08 +0200

    Corregidos los gradientes de utils.py y descomentadas las funciones de main

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ data = [
 batch_name = 'Experimento 01'
 create_comparison_batch(batch_name, data, output_path='./output/')
 
-# # GENERADOR DE LOTES ALEATORIOS
-# create_random_batches(3, variations=2, sequence_name='Aleatorio', sequence_start=2, output_path='./output/')
-#
-# # GENERACION A PARTIR DE ARCHIVO CSV
-# origin_ranges = ((11.3, 15.43), (4.3, 10.67), (7.68, 33.0))
-# tarjet_ranges = ((0.1, 1.0), (0.1, 1.0), (1.2, 2.2))
-# render_csv_file('./datos_reales.csv', 'Epsilon con datos reales', map_origin_ranges=origin_ranges, map_tarjet_ranges=tarjet_ranges,output_path='./output/')
+# GENERADOR DE LOTES ALEATORIOS
+create_random_batches(3, variations=2, sequence_name='Aleatorio', sequence_start=2, output_path='./output/')
+
+# GENERACION A PARTIR DE ARCHIVO CSV
+origin_ranges = ((11.3, 15.43), (4.3, 10.67), (7.68, 33.0))
+tarjet_ranges = ((0.1, 1.0), (0.1, 1.0), (1.2, 2.2))
+render_csv_file('./datos_reales.csv', 'AAAAAAEpsilon con datos reales', map_origin_ranges=origin_ranges, map_tarjet_ranges=tarjet_ranges,output_path='./output/')
